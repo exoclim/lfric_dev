5f2201bcf50a5f936348000aff4bad117163e98e
ricky-lv426 <e.a.olivier@exeter.ac.uk>
2022-11-30 12:14:04 +0000

    Refactoring code for more readability

--- benchmarking/scripts/benchmark_plot.py
+++ benchmarking/scripts/benchmark_plot.py
@@ -1,20 +1,15 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 import argparse
+import json
 
 from pathlib import Path
 from io import StringIO
 
-if __name__ == "__main__":
-    """This script reads in a LFRic benchmarking timer file and plots the top
-    15 most costly routine calls as a histogram plot."""
 
-    parser = argparse.ArgumentParser()
-    parser.add_argument("timer_file_path", help="Location path to timer file")
-    args = parser.parse_args()
+def timer_file_to_dataframe(timer_file_path):
+    """This method read in a LFRic timer file into a pandas data frame"""
 
-    # Process the LFRic timer file into a pandas data frame
-    timer_file_path = Path(args.timer_file_path)
     with open(timer_file_path, "r") as file:
         data = file.read()
         data = data.replace("||", ",")
@@ -25,6 +20,16 @@ if __name__ == "__main__":
     for heading in df.columns:
         headings_dict[heading] = heading.strip()
     df.rename(columns=headings_dict, inplace=True)
+
+    return df
+
+
+def generate_most_expensive_cost_plot(timer_file_path, output_file_name):
+    """This method generates a plot of the top 15 most costly routine
+    calls as a histogram plot."""
+
+    # Process the LFRic timer file into a pandas data frame
+    df = timer_file_to_dataframe(timer_file_path=timer_file_path)
 
     # Sort the dataframe in descending order of expensive routine calls
     df.sort_values(by=["mean time(s)"], inplace=True, ascending=False)
@@ -54,4 +59,19 @@ if __name__ == "__main__":
     ax.set_title("Mean time (s)", loc="left")
 
     # Save plot to file
-    plt.savefig("benchmark_plot.png")
+    plt.savefig(output_file_name + ".png")
+
+
+if __name__ == "__main__":
+    """This script reads in a LFRic benchmarking timer file and plots the top
+    15 most costly routine calls as a histogram plot."""
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("timer_file_path", help="Path to config timings map file")
+    parser.add_argument("output_file_name", help="Path to config timings map file")
+    args = parser.parse_args()
+
+    generate_most_expensive_cost_plot(
+        timer_file_path=Path(args.timer_file_path),
+        output_file_name=args.output_file_name,
+    )
